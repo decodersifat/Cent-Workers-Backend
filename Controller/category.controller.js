aff65feab6854f376752f8446fb92ff006c9e35b
decodersifat <user.sifathossain@gmail.com>
2025-11-11 12:36:34 +0600

    Added Add Category Route

--- Controller/category.controller.js
+++ Controller/category.controller.js
@@ -1,4 +1,31 @@
+import { db } from '../config/db.js';
 
-export const addCategory = (req, res) => {
+export const addCategory = async (req, res) => {
 
+    const { title, uid, image, urlSlug } = req.body;
+
+    try {
+         const category = await db.collection('category').insertOne({
+            title:title,
+            uid:uid,
+            image:image,
+            urlSlug:urlSlug
+         })
+
+         if (!category){
+            return res.status(500).send({
+                "message":"No Category Created",
+            })
+         }
+
+         return res.status(200).send({
+            "message":"Category Created",
+            data:category
+         })
+
+    } catch (error) {
+        return res.status(500).send({
+                "message":error.message
+            })
+    }
 }
--- Routes/category.route.js
+++ Routes/category.route.js
@@ -1,5 +1,8 @@
 import express from 'express';
 
+import { addCategory } from '../Controller/category.controller.js';
 const router = express.Router();
 
-router.post('/add-category', )
+router.post('/add-category', addCategory);
+
+export default router;
